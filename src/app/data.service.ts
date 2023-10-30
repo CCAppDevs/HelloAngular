6ed1c1aee9267ccc464442f6e68b5c8e5900dff9
Jesse Harlan <jesse.harlan@centralia.edu>
2023-10-30 10:52:06 -0700

    added behavior subjects for data service

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+
+export interface Game {
+
+}
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +13,21 @@ export class DataService {
 
   baseURL: string = "htttp://localhost:3000/api";
 
+  private dataArray: any[] = [
+    { name: "toast", id: 1 },
+    { name: "toast 1", id: 2 },
+    { name: "toast 2", id: 3 }
+  ];
+
+  mockGameArray$: BehaviorSubject<any[]> = new BehaviorSubject<any[]>(this.dataArray);
+
   constructor(private http: HttpClient) { }
 
   // get all
+  getAllGames(): void {
+    this.dataArray.push({ name: "New Value", id: 0 });
+    this.mockGameArray$.next(this.dataArray);
+  }
   
   // get one by id
   // create new
